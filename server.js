2d19649555cd3cfbb8ace6486faed8c22860ebdf
samalo <smhonds@gmail.com>
2019-02-04 03:59:54 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+return text.toUpperCase();
+});
+
+app.get('/', (req, res)=>{
+  //res.send('<h1>Hello express</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'About Page',
+    welcomeMessage: 'Welcome to our awesome website!',
+    
+
+  });
+});
+
+app.get('/about', (req, res) =>{
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    
+  });
+});
+
+app.get('/bad', (req, res)=>{
+  res.send({
+  errorMessage: 'Unable to handle the request!'
+  });
+})
+
+app.listen(3000, () => {
+ console.log('Server is up on port 3000');
+});
